a1ade2b44c68f562e15843e2e7403ac29446074d
harishihan-c <harishihan@outlook.com>
2026-01-08 20:54:02 +0530

    feat: add nav menu active logic

--- client/src/components/NavBar/NavBar.jsx
+++ client/src/components/NavBar/NavBar.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './NavBar.css'
 import { assets } from '../../assets/assets'
 
 const NavBar = () => {
+
+    const [menu, setMenu] = useState("home");
+
+
   return (
     <div className="navbar">
         <img src={assets.logo} alt="" className="logo"/>
         <div className="navbar-menu">
             <ul>
-                <li>Home</li>
-                <li>Menu</li>
-                <li>Mobile-app</li>
-                <li>Contact us</li>
+                <li onClick={() => setMenu("home")} className={menu === "home"? "active" : ""}>Home</li>
+                <li onClick={() => setMenu("menu")} className={menu === "menu"? "active" : ""}>Menu</li>
+                <li onClick={() => setMenu("mobile-app")} className={menu === "mobile-app"? "active" : ""}>Mobile-app</li>
+                <li onClick={() => setMenu("contact")} className={menu === "contact"? "active" : ""}>Contact us</li>
             </ul>
         </div>
         <div className="navbar-right">
