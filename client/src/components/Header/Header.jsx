cd24ef845742ebd68bcc625543af4f2ce6b80c40
harishihan-c <harishihan@outlook.com>
2026-01-08 23:15:11 +0530

    feat: add header layout

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import './Header.css'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="header">
+        <div className="header-contents">
+            <h2>Order Your Favourite Food Here</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatum culpa mollitia perspiciatis maiores cumque, officia, debitis odio labore distinctio soluta architecto ad, vitae dolor? Nam accusantium ipsam dolor nihil eum.</p>
+            <button>Sign In</button>
+        </div>
+    </div>
   )
 }
 
